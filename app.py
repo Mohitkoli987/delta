6f96a4939b4200cf5fcbcc30b92aa2f140909306
Mohit koli <mohitkoli9888@gmail.com>
2026-05-04 09:59:26 +0530

    1 0.35

--- app.py
+++ app.py
@@ -141,7 +141,7 @@ DELTA_API_SECRET = os.getenv("DELTA_API_SECRET")
 
 # ---------- LIVE POSITION TP/SL CONFIGURATION ----------
 LIVE_TP_PERCENTAGE = 0.1  # 1% Take Profit
-LIVE_SL_PERCENTAGE = 0.4  # 0.4% Stop Loss
+LIVE_SL_PERCENTAGE = 0.35  # 0.4% Stop Loss
 
 processing_lock = threading.Lock()
 last_processed = {}
